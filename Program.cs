8185c09cd5e6d4d5eab4db39ac569a5247253970
LautaroCruzSrur <lautaro.cruzsrur@gmail.com>
2025-05-20 15:43:16 -0300

    terminando calculadora

--- Program.cs
+++ Program.cs
@@ -10,17 +10,33 @@ switch (opciones)
         int a = int.Parse(Console.ReadLine());
         Console.WriteLine("Ingrese un numero\n");
         int b = int.Parse(Console.ReadLine());
-        int resultado = a + b;
-        Console.WriteLine("El resultado de la suma es:" + resultado);
+        int resultadoSuma = a + b;
+        Console.WriteLine("El resultado de la suma es:" + resultadoSuma);
 
         break;
     case 2:
-
+        Console.WriteLine("Ingrese un numero \n");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese un numero\n");
+        int d = int.Parse(Console.ReadLine());
+        int resultadoResta = c - d;
+        Console.WriteLine("El resultado de la suma es:" + resultadoResta);
+    
         break;
     case 3:
-
+        Console.WriteLine("Ingrese un numero \n");
+        int e = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese un numero\n");
+        int f = int.Parse(Console.ReadLine());
+        int resultadoMultiplicacion = e * f;
+        Console.WriteLine("El resultado de la suma es:" + resultadoMultiplicacion);
         break;
     case 4:
-
+        Console.WriteLine("Ingrese un numero \n");
+        int g = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese un numero\n");
+        int h = int.Parse(Console.ReadLine());
+        int resultadoDivision = g / h;
+        Console.WriteLine("El resultado de la suma es:" + resultadoDivision);
         break;
 }
