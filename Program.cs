e1d3a529c9eac7533d50e24a8e4a8e5d32a60cfe
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-05-23 18:21:21 -0300

    numero invertido

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bienvenido \n Ingrese un numero para invertir");
+string s = Console.ReadLine();
+int n = 0, invertido = 0, aux = 0;
 
-int a;
-int b;
-a=10;
-b=a;
+bool resultado = int.TryParse(s, out n);
 
-Console.WriteLine("valor de a:" + a);
-Console.WriteLine("valor de b:" + b);
+if (resultado)
+{
+    while (n > 0)
+    {
+        aux = n % 10;
+        invertido = invertido * 10 + aux;
+        n /= 10;
+    }
+    
+    Console.WriteLine("El numero invertido es :" + invertido);
+}
+else
+{
+    Console.WriteLine("\n Esto no es un numero");
+}
