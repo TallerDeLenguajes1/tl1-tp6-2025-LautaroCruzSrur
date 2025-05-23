69c4cdca34ae21082b0d21bd17017102f47bb414
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-05-23 19:24:18 -0300

    Terminando Calculadora V1

--- Program.cs
+++ Program.cs
@@ -1,42 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
+bool vuelta = true;
 Console.WriteLine("Hola Bienvenido a la calculadora");
-Console.Write("Ingrese la opcion que desea realizar: \n 1) Suma \n 2)Resta \n 3)Multiplicacion \n 4)Dividir\n");
-int opciones = int.Parse(Console.ReadLine());
-
-switch (opciones)
+while (vuelta)
 {
-    case 1:
-        Console.WriteLine("Ingrese un numero \n");
-        int a = int.Parse(Console.ReadLine());//Usar TryParse
-        Console.WriteLine("Ingrese un numero\n");
-        int b = int.Parse(Console.ReadLine());
-        int resultadoSuma = a + b;
-        Console.WriteLine("El resultado de la suma es:" + resultadoSuma);
+    Console.Write("\nIngrese la opcion que desea realizar: \n 1) Suma \n 2)Resta \n 3)Multiplicacion \n 4)Dividir\n");
+int opciones = 0;
+bool resultado = int.TryParse(Console.ReadLine(), out opciones);
+Console.WriteLine("Ingrese dos números:");
 
-        break;
-    case 2:
-        Console.WriteLine("Ingrese un numero \n");
-        int c = int.Parse(Console.ReadLine());
-        Console.WriteLine("Ingrese un numero\n");
-        int d = int.Parse(Console.ReadLine());
-        int resultadoResta = c - d;
-        Console.WriteLine("El resultado de la suma es:" + resultadoResta);
-    
-        break;
-    case 3:
-        Console.WriteLine("Ingrese un numero \n");
-        int e = int.Parse(Console.ReadLine());
-        Console.WriteLine("Ingrese un numero\n");
-        int f = int.Parse(Console.ReadLine());
-        int resultadoMultiplicacion = e * f;
-        Console.WriteLine("El resultado de la suma es:" + resultadoMultiplicacion);
-        break;
-    case 4:
-        Console.WriteLine("Ingrese un numero \n");
-        int g = int.Parse(Console.ReadLine());
-        Console.WriteLine("Ingrese un numero\n");
-        int h = int.Parse(Console.ReadLine());
-        int resultadoDivision = g /h;
-        Console.WriteLine("El resultado de la suma es:" + resultadoDivision);
-        break;
+Console.Write("Primer número: ");
+bool validoA = int.TryParse(Console.ReadLine(), out int a);
+
+Console.Write("Segundo número: ");
+bool validoB = int.TryParse(Console.ReadLine(), out int b);
+
+if (resultado && validoA && validoB)
+{
+        switch (opciones)
+        {
+            case 1:
+                int resultadoSuma = a + b;
+                Console.WriteLine("El resultado de la suma es:" + resultadoSuma);
+                break;
+            case 2:
+                int resultadoResta = a - b;
+                Console.WriteLine("El resultado de la suma es:" + resultadoResta);
+                break;
+            case 3:
+                int resultadoMultiplicacion = a * b;
+                Console.WriteLine("El resultado de la suma es:" + resultadoMultiplicacion);
+                break;
+            case 4:
+                int resultadoDivision = a / b;
+                Console.WriteLine("El resultado de la suma es:" + resultadoDivision);
+                break;
+        }
+        Console.WriteLine("Desea Realizar otro calculo? Y/N");
+        string bandera = Console.ReadLine();
+        if (bandera == "N" || bandera == "n")
+        {
+            vuelta = false;
+        }
+    }else
+{
+    Console.WriteLine("Uno o ambos valores no son números válidos.");
+}        
 }
+
+
+
